e59ac7480ecf1fb7728d2d8d45c40c67cc0f34e8
Axel Garcia <axel.garcia94@gmail.com>
2019-02-26 21:47:30 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const main = () => {
+  const typedElement = document.querySelector('#typed')
+  const typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    loop: true,
+    typeSpeed: 100,
+  });
+
+  const scrollDown = () => {
+    window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
+  };
+  const arrowDown = document.querySelector('.arrow-section');
+  arrowDown.addEventListener('click', scrollDown);
+};
+
+window.addEventListener('load', main);
