660b041b68ae1e4b56c22f432c633b1b77318a9d
Axel Garcia <axel.garcia94@gmail.com>
2019-03-04 16:46:08 +0100

    titles animations

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 'use strict';
 
 const main = () => {
+  const snapContainer = document.querySelector('#site-main');
+  const listSectionTitles = document.querySelectorAll('.section-title');
+  const titlesElements = []
+
+  listSectionTitles.forEach((element) => {
+    titlesElements.push(element.getBoundingClientRect())
+  })
+  snapContainer.addEventListener('scroll', () => {
+    titlesElements.forEach((title, index) => {
+      const position = index + 1; 
+      if(title.top > window.innerHeight*position && title.top < window.innerHeight*(position + 1) && snapContainer.scrollTop === window.innerHeight*position) {
+        listSectionTitles[index].classList.add('slide-in-left');
+      }
+
+      if (title.top > window.innerHeight*position && title.top < window.innerHeight*(position + 1) && snapContainer.scrollTop >= window.innerHeight*(position+1)) {
+        listSectionTitles[index].classList.remove('slide-in-left');
+      }
+
+      if (title.top > window.innerHeight*position && title.top < window.innerHeight*(position + 1) && snapContainer.scrollTop < window.innerHeight*(position-0.5)) {
+        listSectionTitles[index].classList.remove('slide-in-left');
+      }
+    })
+  })
 
   window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
@@ -27,12 +50,12 @@ const main = () => {
   setTypedBehaviour('#typed')
   setCarousel('.glide')
 
-  const scrollDown = () => {
-    window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
-  };
-  const arrowDown = document.querySelector('.arrow-section');
+  // const scrollDown = () => {
+  //   window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
+  // };
 
-  arrowDown.addEventListener('click', scrollDown);
+  // const arrowDown = document.querySelector('.arrow-section');
+  // arrowDown.addEventListener('click', scrollDown);
 };
 
 window.addEventListener('load', main);
