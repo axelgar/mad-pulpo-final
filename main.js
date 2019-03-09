6ca9b4797ecbb1478acd7abb38d8457330631936
Axel Garcia <axel.garcia94@gmail.com>
2019-03-09 15:01:17 +0100

    uncomment sesize vh units ja

--- main.js
+++ main.js
@@ -56,10 +56,10 @@ const main = () => {
     listSectionTitles[index].classList.remove('slide-in-left');
   }
 
-  // window.addEventListener('resize', () => {
-  //   let vh = window.innerHeight * 0.01;
-  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  // });
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
   const setTypedBehaviour = (domElement) => {
     new Typed(domElement, {
