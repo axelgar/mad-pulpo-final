6c821dd5e5ef143908c1bc8453f3e14c590dd089
axelgar <axel.garcia94@gmail.com>
2019-11-01 13:21:39 +0100

    adding new style guide to site

--- main.js
+++ main.js
@@ -1,69 +1,61 @@
 'use strict';
 
 const main = () => {
-  const snapContainer = document.querySelector('#site-main');
-  const listSectionTitles = document.querySelectorAll('h2.section-title');
-  const titlesElements = [];
-  const projectsSection = document.querySelector('#projects');
+  // const snapContainer = document.querySelector('#site-main');
+  // const listSectionTitles = document.querySelectorAll('h2.section-title');
+  // const titlesElements = [];
+  // const projectsSection = document.querySelector('#projects');
 
-  listSectionTitles.forEach((element) => {
-    titlesElements.push(element.getBoundingClientRect())
-  })
+  // listSectionTitles.forEach((element) => {
+  //   titlesElements.push(element.getBoundingClientRect())
+  // })
 
-  snapContainer.addEventListener('scroll', () => {
-    titlesElements.forEach((title, index) => {
-      let VIEW_CURRENT_POSITION = index + 1;
-      if (index === 4) {
-        VIEW_CURRENT_POSITION = index + projectsSection.clientHeight / window.innerHeight - 0.5;
-      }
-      const VIEW_NEXT_POSITION =  VIEW_CURRENT_POSITION + 1;
-      const VIEW_PREVIOUS_POSITION =  VIEW_CURRENT_POSITION - 0.9;
-      const heigth = snapContainer.clientHeight;
-      const topViewPosition = heigth * VIEW_CURRENT_POSITION ;
-      const bottomViewPosition = heigth * VIEW_NEXT_POSITION;
-      const previoysPosition = heigth * VIEW_PREVIOUS_POSITION;
+  // snapContainer.addEventListener('scroll', () => {
+  //   titlesElements.forEach((title, index) => {
+  //     let VIEW_CURRENT_POSITION = index + 1;
+  //     if (index === 4) {
+  //       VIEW_CURRENT_POSITION = index + projectsSection.clientHeight / window.innerHeight - 0.5;
+  //     }
+  //     const VIEW_NEXT_POSITION =  VIEW_CURRENT_POSITION + 1;
+  //     const VIEW_PREVIOUS_POSITION =  VIEW_CURRENT_POSITION - 0.9;
+  //     const heigth = snapContainer.clientHeight;
+  //     const topViewPosition = heigth * VIEW_CURRENT_POSITION ;
+  //     const bottomViewPosition = heigth * VIEW_NEXT_POSITION;
+  //     const previoysPosition = heigth * VIEW_PREVIOUS_POSITION;
 
-      if (checkTitleInView(title, topViewPosition, bottomViewPosition)) {
-        addClassAnimation(index);
-      }
+  //     if (checkTitleInView(title, topViewPosition, bottomViewPosition)) {
+  //       addClassAnimation(index);
+  //     }
 
-      if (chechkTitleOutOfView(title, topViewPosition, bottomViewPosition, previoysPosition)) {
-        removeClassAnimation(index);
-      }
-    })
-  })
+  //     if (chechkTitleOutOfView(title, topViewPosition, bottomViewPosition, previoysPosition)) {
+  //       removeClassAnimation(index);
+  //     }
+  //   })
+  // })
 
-  function checkTitleInView(title, topPosition, bottomPosition) {
-    if(title.top > topPosition && title.top < bottomPosition) {
-      return true;
-    }
-  }
+  // function checkTitleInView(title, topPosition, bottomPosition) {
+  //   if(title.top > topPosition && title.top < bottomPosition) {
+  //     return true;
+  //   }
+  // }
 
-  function chechkTitleOutOfView(title, topPosition, bottomPosition, previousPosition) {
-    if (title.top > topPosition && title.top < bottomPosition && snapContainer.scrollTop >= bottomPosition) {
-      return true;
-    }
+  // function chechkTitleOutOfView(title, topPosition, bottomPosition, previousPosition) {
+  //   if (title.top > topPosition && title.top < bottomPosition && snapContainer.scrollTop >= bottomPosition) {
+  //     return true;
+  //   }
 
-    if (title.top > topPosition && title.top < bottomPosition && snapContainer.scrollTop < previousPosition) {
-      return true;
-    }
-  }
+  //   if (title.top > topPosition && title.top < bottomPosition && snapContainer.scrollTop < previousPosition) {
+  //     return true;
+  //   }
+  // }
 
-  function addClassAnimation(index) {
-    listSectionTitles[index].classList.add('slide-in-left');
-  }
+  // function addClassAnimation(index) {
+  //   listSectionTitles[index].classList.add('slide-in-left');
+  // }
 
-  function removeClassAnimation(index) {
-    listSectionTitles[index].classList.remove('slide-in-left');
-  }
-
-  const setTypedBehaviour = (domElement) => {
-    new Typed(domElement, {
-      stringsElement: '#typed-strings',
-      loop: true,
-      typeSpeed: 100,
-    });
-  }
+  // function removeClassAnimation(index) {
+  //   listSectionTitles[index].classList.remove('slide-in-left');
+  // }
 
   const setCarousel = (element) => {
     new Glide(element, {
@@ -74,15 +66,7 @@ const main = () => {
     }).mount()
   }
 
-  setTypedBehaviour('#typed')
   setCarousel('.glide')
-
-  // const scrollDown = () => {
-  //   window.scrollBy({ top: window.innerHeight, behavior: 'smooth' });
-  // };
-
-  // const arrowDown = document.querySelector('.arrow-section');
-  // arrowDown.addEventListener('click', scrollDown);
 };
 
 window.addEventListener('load', main);
